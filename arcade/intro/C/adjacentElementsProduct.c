3e2c5308c47b671349d97e367e39f4ac19879b6e
Kishan Gondaliya <kishan.gondaliya@softnautics.com>
2018-09-21 17:33:12 +0530

    Add arcade intro C solutions
    Signed-off-by: Kishan Gondaliya <kishan.gondaliya@softnautics.com>

---
+++ arcade/intro/C/adjacentElementsProduct.c
@@ -0,0 +1,8 @@
+int adjacentElementsProduct(arr_integer i) {
+    int t=i.arr[0]*i.arr[1], m=0;
+    while (--i.size) {
+        if (t<i.arr[m]*i.arr[m+1]) t = i.arr[m]*i.arr[m+1];
+        m++;
+    }
+    return t;
+}
---
+++ arcade/intro/C/matrixElementsSum.c
@@ -0,0 +1,13 @@
+int matrixElementsSum(arr_arr_integer m) {
+    int r=0,i=0,j;
+
+    for (; i<m.arr[0].size;i++) {
+        for(j=0;j<m.size;j++) {
+            if (m.arr[j].arr[i])
+                r+=m.arr[j].arr[i];
+            else break;
+        }
+    }
+    return r;
+}
+
