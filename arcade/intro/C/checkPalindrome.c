6e200052085e00a3a2fc29f9b3116f8dce13dca9
Kishan Gondaliya <kishan.gondaliya@softnautics.com>
2018-11-22 15:11:40 +0530

    Change indent
    Signed-off-by: Kishan Gondaliya <kishanpgondaliya@gmail.com>

--- arcade/intro/C/checkPalindrome.c
+++ arcade/intro/C/checkPalindrome.c
@@ -1,5 +1,5 @@
 bool checkPalindrome(char * inputString) {
-    int len = strlen(inputString);
-    for (int i = 0; i < len/2; i++) if(inputString[i] != inputString[len-i -1]) return false;
-    return true;
+	int len = strlen(inputString);
+	for (int i = 0; i < len/2; i++) if (inputString[i] != inputString[len-i -1]) return false;
+	return true;
 }
